dde58a7231c0aa289792bc47ad4e9fa3bfc9073a
Sammm2006 <sasindupabasara1@gmail.com>
2026-03-19 15:46:28 +0800

    ReloadFunction

--- game.py
+++ game.py
@@ -11,14 +11,19 @@ C_BG = (5,8,18)
 C_PLAYER = (0,220,255)
 C_BULLET = (0, 255, 162)
 C_SHOOT = (255,255,255)
+C_DOT_ON = (0, 255, 180)
+C_DOT_OFF= (30, 50, 60)
+C_RECHARGE = (0, 255, 162)
 
 x,y = 320, 240 # Spawn Position
-SIZE = 15
+SIZE = 18
 SPEED = 3
+MAX_BULLETS  = 10
 bullets = []
 shoot_timer = 0
 shoot_flash = 0
 shake = 0
+ammo = 5 
 
 # Functions
 def angle_to(ox, oy, tx, ty):
@@ -53,13 +58,22 @@ while True:
         shake -= 1
 
     if mouse[0] and shoot_timer == 0:
-        angle = angle_to(x, y, mx, my)
-        vx = math.cos(angle) * 8
-        vy = math.sin(angle) * 8
-        bullets.append([x,y,vx,vy])
-        shoot_timer = 20
-        shoot_flash = 10
-        shake = 8
+        click_on_player = math.hypot(mx - x, my - y) < SIZE
+
+        if click_on_player:
+            ammo = min(ammo + 1, MAX_BULLETS)   # add 1, but never go above 5
+            shoot_timer = 10
+            player_color = C_RECHARGE
+        
+        elif ammo > 0:
+            angle = angle_to(x, y, mx, my)
+            vx = math.cos(angle) * 8
+            vy = math.sin(angle) * 8
+            bullets.append([x,y,vx,vy])
+            shoot_timer = 20
+            shoot_flash = 10
+            shake = 8
+            ammo -= 1
 
     for b in bullets[:]:
         b[0] += b[2]
@@ -80,4 +94,14 @@ while True:
     pygame.draw.circle(screen, player_color, (x + ox, y + oy), SIZE)
     for b in bullets:
         pygame.draw.circle(screen, C_BULLET, (int(b[0]), int(b[1])), 5)
+
+    dot_radius  = 6
+    dot_spacing = 20
+    total_width = (MAX_BULLETS - 1) * dot_spacing
+    start_x     = 400 - total_width // 2   # centered on screen
+
+    for i in range(MAX_BULLETS):
+        color = C_DOT_ON if i < ammo else C_DOT_OFF
+        pygame.draw.circle(screen, color, (start_x + i * dot_spacing, 580), dot_radius)
+    
     pygame.display.flip()
--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-    if mouse[0] and shoot_timer == 0:
-        angle = angle_to(x, y, mx, my)
-        vx = math.cos(angle) * 8
-        vy = math.sin(angle) * 8
-        bullets.append([x, y, vx, vy])
-        shoot_timer = 10
